3348428f4992de3d8019e84deb2df49ef25a3fbc
Luca <lucamonk1234@gmail.com>
2015-10-02 00:16:15 +1000

    updated user profile url
    /u/{{username}} needs fixing

--- stories/urls.py
+++ stories/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     url(r'^$', 'stories.views.index'),
     url(r'^story/$', 'stories.views.story'),
     url(r'^vote/$', 'stories.views.vote'),
-    url(r'^(?P<category_name>\w+)/$', 'stories.views.category'),
-    url(r'^(?P<category_name>\w+)/(?P<story_id>[0-9]+)/$', 'stories.views.comment'),
+    url(r'^s/(?P<category_name>\w+)/$', 'stories.views.category'),
+    url(r'^s/(?P<category_name>\w+)/(?P<story_id>[0-9]+)/$', 'stories.views.comment'),
+    url(r'^u/(?P<username>\w+)/$', 'stories.views.profile'),
 ]
