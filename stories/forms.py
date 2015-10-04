ccff70570ac739127b6db9989e12aad4917f993b
Luca <lucamonk1234@gmail.com>
2015-10-04 20:22:29 +1100

    got rid of comments for now

--- stories/forms.py
+++ stories/forms.py
@@ -20,7 +20,6 @@ class UserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
